c10cbeede5a698c3570bae624140187313e0c8b9
7savv7 <schrysostomid@gmail.com>
2026-06-07 22:02:29 +0300

    Create feed model and table

---
+++ database/migrations/2026_06_07_182940_create_feeds_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feeds', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+
+            $table->string("url");
+            $table->string("title");
+            $table->text("description");
+            $table->string("favicon");
+
+            $table->string("custom_title")->nullable();
+            $table->string("category")->nullable();
+
+            $table->timestamp("last_fetch_time");
+            $table->enum("last_health_status", ["active", "error"])->nullable();
+            $table->enum("health_status", ["active", "stale", "error"])->default("active");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feeds');
+    }
+};

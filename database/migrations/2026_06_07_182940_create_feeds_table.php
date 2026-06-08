3328f4ff781577aa7f21921a50eaf1e9e043d5af
7savv7 <schrysostomid@gmail.com>
2026-06-08 16:08:07 +0300

    Create categories table and model

--- database/migrations/2026_06_07_182940_create_feeds_table.php
+++ database/migrations/2026_06_07_182940_create_feeds_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
             $table->id();
             $table->foreignId("user_id")->constrained()->onDelete("cascade");
 
-            $table->string("url");
+            $table->string("url")->unique();
             $table->string("title");
             $table->text("description");
             $table->string("favicon");
 
             $table->string("custom_title")->nullable();
-            $table->string("category")->nullable();
+            $table->foreignId("category_id")->nullable()->constrained()->nullOnDelete();
 
-            $table->timestamp("last_fetch_time");
+            $table->timestamp("last_fetch_at")->nullable();
             $table->enum("last_health_status", ["active", "error"])->nullable();
             $table->enum("health_status", ["active", "stale", "error"])->default("active");
             $table->timestamps();
